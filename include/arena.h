f90d2ee01870feb215611332609d42aa9f46bde2
Chris Schmidt <cmatthewschmidt@gmail.com>
2023-02-01 02:10:36 -0500

    create arena menu

---
+++ include/arena.h
@@ -0,0 +1,2 @@
+
+void arena_menu();
---
+++ src/arena.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "arena.h"
+#include "utility.h"
+//#include "Enemy.h"
+
+void arena_menu() {
+    pairVec menu_items;
+
+    do {
+        std::cout << "Welcome to the arena! You can expect formidable foes if you dare to fight.\n\n";
+        std::cout << "[Arena Menu]\n";
+
+        menu_items.clear();
+
+        menu_items.push_back(std::make_pair("Fight", []() -> void {
+            //Enemy::generate_enemy();
+            std::cout << "The fight starts!\n";
+        }));
+
+        menu_items.push_back(std::make_pair("Train", []() -> void {
+            std::cout << "You will have to pay gold to train.\n";
+        }));
+
+    } while(print_menu(menu_items));
+}
