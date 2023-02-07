94df8b63f8cadcf3188412477c9f29469ed74be3
Chris Schmidt <cmatthewschmidt@gmail.com>
2023-02-06 21:23:11 -0500

    make some Arena methods private

--- include/Arena.h
+++ include/Arena.h
@@ -6,11 +6,6 @@
 
 struct Arena {
     void main_menu();
-    void fight_menu();
-    void train_menu();
-    void pre_round_checks(Combat &fighter);
-    void process_attack(Combat &attacker, Combat &defender);
-    void print_stats();
 private:
     // Some choices in fight/train menu should not cause a new move to be rolled
     // This will also prevent pre_round_checks from running unnecessarily
@@ -18,4 +13,10 @@ private:
     bool continue_round = true;
     Combat &player = Player::get();
     Enemy enemy = Enemy::generate_enemy(Player::get().get_stat("Level"));;
+
+    void fight_menu();
+    void train_menu();
+    void pre_round_checks(Combat &fighter);
+    void process_attack(Combat &attacker, Combat &defender);
+    void print_stats();
 };
