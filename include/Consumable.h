7f5c2b51dc7ee5f69cb45d2d11dc4b12a3e6fa8c
Chris Schmidt <cmatthewschmidt@gmail.com>
2023-02-08 05:33:46 -0500

    update Consumable to work with new assets

--- include/Consumable.h
+++ include/Consumable.h
@@ -3,17 +3,18 @@
 #include "Item.h"
 
 struct Consumable : Item {
-    const unsigned int ATTACK_MOD;
-    const unsigned int DEFENSE_MOD;
-    const unsigned int HEALTH_REGEN;
+    const int ATTACK_MOD;
+    const int ACCURACY_MOD;
+    const int DEFENSE_MOD;
+    const int HEALTH_REGEN;
 
-    Consumable(unsigned int id, std::string item_name, VenType type,
-        unsigned int price, unsigned int tier, Rarity rarity, bool can_drop,
-        unsigned int attack_mod, unsigned int defense_mod,
-        unsigned int health_regen): 
+    Consumable(int id, std::string item_name, VenType type,
+        int price, int tier, Rarity rarity, bool can_drop,
+        int attack_mod, int accuracy_mod,
+        int defense_mod, int health_regen): 
         Item(id, item_name, type, price, tier, rarity, can_drop),
-        ATTACK_MOD{attack_mod}, DEFENSE_MOD(defense_mod),
-        HEALTH_REGEN(health_regen) {};
+        ATTACK_MOD{attack_mod}, ACCURACY_MOD(accuracy_mod),
+        DEFENSE_MOD(defense_mod), HEALTH_REGEN(health_regen) {};
     
     void display_item_options() override;
 };
